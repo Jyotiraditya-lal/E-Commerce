1b3a96bc1c50eb9f232868f068d6384c34175f57
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-09-07 16:10:30 +0530

    Delete Mail-Box directory

--- Mail-Box/src/Components/Home/Home.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from './Home';
-
-test('renders welcome message', () => {
-  render(<Home />);
-  const welcomeMessage = screen.getByText('Welcome to your mail box');
-  expect(welcomeMessage).toBeInTheDocument();
-});
