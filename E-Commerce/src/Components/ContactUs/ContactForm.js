b942325a3c106e68dee4528d80bf37a64158a93a
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2024-11-18 18:46:14 +0530

    Design changes

--- E-Commerce/src/Components/ContactUs/ContactForm.js
+++ E-Commerce/src/Components/ContactUs/ContactForm.js
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import { Button } from 'react-bootstrap';
 
 import classes from './ContactForm.module.css';
 
@@ -11,20 +12,26 @@ function ContactForm(props) {
     
     event.preventDefault();
 
-    const info = {
-      name: NameRef.current.value,
-      emailId: EmailRef.current.value,
-      phoneNumber: NumberRef.current.value,
-    };
-    NameRef.current.value=''
-    EmailRef.current.value=''
-    NumberRef.current.value=''
-
-    props.onSendInfo(info);
+    if(!EmailRef.current.value.includes('@') && !EmailRef.current.value.includes('.com')){
+      alert('Please enter a valid email')
+    }else if(NumberRef.current.value.length!==10){
+      alert('Please enter a valid phone number')
+    }else{
+      const info = {
+        name: NameRef.current.value,
+        emailId: EmailRef.current.value,
+        phoneNumber: NumberRef.current.value,
+      };
+      NameRef.current.value=''
+      EmailRef.current.value=''
+      NumberRef.current.value=''
+  
+      props.onSendInfo(info);
+    }
   }
 
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} style={{paddingLeft: '13rem',paddingRight: '13rem',paddingTop: '30px'}} >
       <div className={classes.control}>
         <label htmlFor='name'>Name:</label>
         <input type='text' id='name' ref={NameRef} />
@@ -37,7 +44,7 @@ function ContactForm(props) {
         <label htmlFor='number'>Phone Number:</label>
         <input type='number' id='number' ref={NumberRef} />
       </div>
-      <button>Send Info</button>
+      <Button type='submit' className='btn btn-secondary'>Send Info</Button>
     </form>
   );
 }
