35af6ca88ed5a932a61d193538c4c204951b6e48
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2024-11-18 18:39:43 +0530

    Design changes and added merch

--- E-Commerce/src/Components/Store/Store.js
+++ E-Commerce/src/Components/Store/Store.js
@@ -7,7 +7,7 @@ import ContextProvider from "./Context/ContextProvider";
 import CartButton from "./Cart/CartButton";
 import { NavLink } from "react-router-dom";
 import LoginContext from "../Login/Context/login-context";
-
+import Merch from './Products/Merch'
 
 
 const Store=()=>{
@@ -24,31 +24,33 @@ const Store=()=>{
     if(ctx.isLoggedin){
        bttn=<Button onClick={ctx.logout} variant="primary">Logout</Button>
     }else{
-       bttn=<NavLink style={{color: 'white'}} to="/login">Login</NavLink>
+       bttn=<NavLink style={{color: 'white',textDecoration: 'none'}} to="/login">Login</NavLink>
     }
 
   return(
     <ContextProvider>
       <Navbar bg="dark" expand="sm" variant="dark">
         <Container>
-            <NavLink style={{color: 'white'}} to="/">Home</NavLink>
-            <NavLink style={{color: 'white'}} to="/Store">Shop</NavLink>
-            <NavLink style={{color: 'white'}} to="/About">About Us</NavLink>
-            <NavLink style={{color: 'white'}} to="/ContactUs">Contact Us</NavLink>
+            <NavLink style={{color: 'white',textDecoration: 'none'}} to="/">Home</NavLink>
+            <NavLink style={{color: 'white',textDecoration: 'none'}} to="/Store">Shop</NavLink>
+            <NavLink style={{color: 'white',textDecoration: 'none'}} to="/About">About Us</NavLink>
+            <NavLink style={{color: 'white',textDecoration: 'none'}} to="/ContactUs">Contact Us</NavLink>
             <CartButton onShow={StateChangeHandler} />
             {bttn}
         </Container>
       </Navbar>
       <div >
       <Navbar bg="secondary" variant="dark" className="mb-3" >
-        <Container style={{height: "15rem"}}>
-          <h2 className="center-navbar">The Generics</h2>
-        </Container>
+      <div style={{height: "15rem"}}>
+        <h2 className="center-navbar" style={{marginLeft: '30rem',marginTop:'3rem'}}>The Generics</h2>
+      </div>
       </Navbar>
       </div>
       {ShowCart && <Cart onCancel={StateChangeHandler}/>}
       <p style={{fontFamily: "cursive", fontSize: "50px", textAlign: "center"}}>Music</p>
       <Products />
+      <p style={{fontFamily: "cursive", fontSize: "50px", textAlign: "center"}}>Merch</p>
+      <Merch />
     </ContextProvider>
   )
 }
