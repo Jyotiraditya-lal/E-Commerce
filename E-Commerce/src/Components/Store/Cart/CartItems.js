32c61000164460ec733f623520d7ce47a68b5935
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2024-11-18 18:37:01 +0530

    Design changes

--- E-Commerce/src/Components/Store/Cart/CartItems.js
+++ E-Commerce/src/Components/Store/Cart/CartItems.js
@@ -1,22 +1,45 @@
 import React from "react";
-import { Button, Card, Col, Row } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
-const CartItems=(props)=>{
-    
-    return(
-        <Card style={{width: "30rem"}}>
-            <Card.Body>
-                <Card.Img src={props.url} style={{width: "5rem"}}/>
-                <Row xs={3}>
-                   <Col>{props.title}</Col>
-                   <Col>${props.price}</Col>
-                   <Col>{props.amount}
-                   <Button className="ms-3" variant="danger" onClick={props.onRemove}>Remove</Button>
-                   </Col> 
-                </Row>
-            </Card.Body>
-        </Card>
-    )
-}
+const CartItems = (props) => {
+  return (
+    <Card style={{ width: "30rem", marginBottom: "1rem",marginLeft: '5rem',border: 'none' }}>
+      <Card.Body style={{ display: "flex", alignItems: "center" }}>
+        <Card.Img
+          src={props.url}
+          style={{ width: "5rem", height: "5rem", marginRight: "1rem" }}
+        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexGrow: 1,
+          }}
+        >
+          <span style={{ flex: 1 }}>{props.title}</span>
+          <span style={{ flex: 1 }}>${props.price}</span>
+          <span style={{ flex: 1 }}>
+            {props.amount}
+            <span
+              style={{
+                color: "red",
+                cursor: "pointer",
+                marginLeft: "0.5rem",
+                fontWeight: "bold",
+                fontSize: '1.5rem',
+                marginLeft:'3rem'
+              }}
+              onClick={props.onRemove}
+              title="Remove"
+            >
+              -
+            </span>
+          </span>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
 
-export default CartItems
+export default CartItems;
