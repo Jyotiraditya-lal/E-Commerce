625c763fede956dd6d1aad11c9195a65e45256ed
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2024-11-18 18:35:02 +0530

    added message for empty cart

--- E-Commerce/src/Components/Store/Cart/Cart.js
+++ E-Commerce/src/Components/Store/Cart/Cart.js
@@ -1,53 +1,63 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 import CartItems from "./CartItems";
-import './Cart.css'
+import "./Cart.css";
 import Context from "../Context/context";
 import CartModal from "../UI/Modal";
 import { Button } from "react-bootstrap";
 
+const Cart = (props) => {
+  const Cartctx = useContext(Context);
 
+  const isEmptyCart = Cartctx.items.length === 0;
 
-const Cart=(props)=>{
+  const totalAmount = Cartctx.items.reduce(
+    (total, item) => total + item.price * item.amount,
+    0
+  );
 
-    const Cartctx=useContext(Context)
+  const RemoveHandler = (id) => {
+    Cartctx.RemoveItems(id);
+  };
 
-    const RemoveHandler=(id)=>{
-        Cartctx.RemoveItems(id)
-    }
-
-
-    let totalAmount=0
-    for(let i=0;i<Cartctx.items.length;i++){
-        totalAmount=totalAmount+ ((Cartctx.items[i].price)*(Cartctx.items[i].amount))
-    }
-
-    const cartitems=<ul className="cart-items">{Cartctx.items.map((product)=>
-            <CartItems 
-               key={product.id}
-               url={product.imageUrl}
-               title={product.title}
-               price={product.price}
-               amount={product.amount}
-               onRemove={RemoveHandler.bind(null,product.id)}
+  return (
+    <CartModal onRemove={props.onCancel}>
+      <header>
+        <Button
+          className="btn btn-danger"
+          style={{ marginLeft: "35rem" }}
+          onClick={props.onCancel}
+        >
+          x
+        </Button>
+      </header>
+      <p style={{ fontFamily: "cursive", textAlign: "center",fontSize: '2rem' }}>Your Cart</p>
+      {!isEmptyCart && (
+        <ul className="cart-items">
+          {Cartctx.items.map((product) => (
+            <CartItems
+              key={product.id}
+              url={product.imageUrl}
+              title={product.title}
+              price={product.price}
+              amount={product.amount}
+              onRemove={RemoveHandler.bind(null, product.id)}
             />
-        )}</ul>
-    return(
-        <CartModal onRemove={props.onCancel}>
-            <header>
-                <Button variant="light" onClick={props.onCancel}>x</Button>
-            </header>
-            <p style={{fontFamily: "cursive", textAlign: "center"}}>Cart</p>
-            {cartitems}
-            <div>
-                <span>Total Amount:   ${totalAmount}</span>
-                <br />
-                <br />
-                <span>
-                    <Button onClick={Cartctx.purchase}>Purchase</Button>
-                </span>
-            </div>
-        </CartModal>
-    )
-}
+          ))}
+        </ul>
+      )}
+      {!isEmptyCart && (
+        <div>
+          <span style={{marginLeft:'25rem'}}>Total Amount: ${totalAmount.toFixed(2)}</span>
+          <br />
+          <br />
+          <span>
+            <Button onClick={Cartctx.purchase} style={{marginLeft:'29rem'}}>Purchase</Button>
+          </span>
+        </div>
+      )}
+      {isEmptyCart && <p style={{marginLeft: '10rem',color: 'red'}}>Add items to be displayed on the cart</p>}
+    </CartModal>
+  );
+};
 
-export default Cart
+export default Cart;
