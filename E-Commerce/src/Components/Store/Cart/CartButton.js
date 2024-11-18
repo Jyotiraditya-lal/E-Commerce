bd633c5d5a29e8ec89a1486d5ac32ead2b79e84c
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2024-11-18 18:36:20 +0530

    Resolved a bug that shows NaN instead od a number

--- E-Commerce/src/Components/Store/Cart/CartButton.js
+++ E-Commerce/src/Components/Store/Cart/CartButton.js
@@ -1,19 +1,24 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 import { Button } from "react-bootstrap";
 import Context from "../Context/context";
 
-const CartButton=(props)=>{
+const CartButton = (props) => {
+  const Cartctx = useContext(Context);
 
-    const Cartctx=useContext(Context)
+  const number = Cartctx.items.reduce((total, item) => {
+    const amount = Number(item.amount) || 0;
+    return total + amount;
+  }, 0);
 
-    let number=0;
-    for(let i=0;i<Cartctx.items.length;i++){
-        number+=Number(Cartctx.items[i].amount)
-    }
+  return (
+    <Button
+      variant="dark"
+      onClick={props.onShow}
+      style={{ borderColor: "blue" }}
+    >
+      Cart({number})
+    </Button>
+  );
+};
 
-    return(
-        <Button variant="dark" onClick={props.onShow} style={{borderColor: "blue"}}>Cart({number})</Button>
-    )
-}
-
-export default CartButton
+export default CartButton;
